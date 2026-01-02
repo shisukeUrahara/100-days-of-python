cd72acc0451e428f7e50f9d32d2a545cdc416ddb
Abhishek Sagu <abhisheksagu@gmail.com>
2026-01-02 19:46:21 +0530

    day-1 of python:100 days of code completed

---
+++ day-1/main.py
@@ -0,0 +1,16 @@
+# Write your code below this line 👇
+from xml.etree.ElementTree import tostring
+
+print("Hello World!")
+# ########################################
+# print("Hello "+input("Enter your name")+"!")
+# #######################################
+# name= input("What is your name?")
+# print("Hello " + name)
+# ##########################################
+# name = input("whats your name?")
+# print("your name is "+len(name).__str__()+" characters long")
+#  #########################################################
+city_name=input("what city did you grow up in?\n")
+pet_name= input("what is your pet name?\n")
+print("your band name should be "+city_name+" "+pet_name)
